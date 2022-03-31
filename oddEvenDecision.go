08780b26bbaf8b1999c45da99d5537f50ca4e04a
Azri Rahmat <azri-rahmat@Azris-MacBook-Pro.local>
2022-03-31 23:33:22 +0800

    adding practice exercise solution

--- oddEvenDecision.go
+++ oddEvenDecision.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert code here
+	var userInput1 int
+
+	for {
+		fmt.Println("Please enter a number")
+		fmt.Scanln(&userInput1)
+
+		if userInput1%2 == 0 {
+			fmt.Println("This is a even number")
+		} else {
+			fmt.Println("This is a odd number")
+		}
+	}
 }
